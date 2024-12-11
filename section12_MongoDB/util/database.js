8f8d27ee758a9969b62d055e43c735b3e2b9978c
praveendan <38735067+praveendan@users.noreply.github.com>
2024-12-10 20:39:07 -0600

    section12_mongo - AppJS update and established connection

--- section12_MongoDB/util/database.js
+++ section12_MongoDB/util/database.js
@@ -4,25 +4,25 @@ const MongoClient = mongodb.MongoClient;
 let _db;
 
 const mongoConnect = callback => {
-  MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
-  )
-    .then(client => {
-      console.log('Connected!');
-      _db = client.db();
-      callback();
-    })
-    .catch(err => {
-      console.log(err);
-      throw err;
-    });
+    MongoClient.connect(
+        'mongodb+srv://praveendan:fW9WF1TbIMGZBKWQ@nodejs-course.27hsh.mongodb.net/?retryWrites=true&w=majority&appName=NodeJS-course'
+    )
+        .then(client => {
+            console.log('Connected!');
+            _db = client.db();
+            callback();
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
 };
 
 const getDb = () => {
-  if (_db) {
-    return _db;
-  }
-  throw 'No database found!';
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found!';
 };
 
 exports.mongoConnect = mongoConnect;
