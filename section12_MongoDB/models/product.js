228d3aa9b202cc4ff9a09ee609423255ef8f19b8
praveendan <38735067+praveendan@users.noreply.github.com>
2025-01-04 14:55:34 -0600

    section12 mongo delete functionality

--- section12_MongoDB/models/product.js
+++ section12_MongoDB/models/product.js
@@ -1,6 +1,5 @@
 const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
-const mongodb = require('mongodb')
 
 class Product {
   constructor(title, price, description, imageUrl, id) {
@@ -55,6 +54,19 @@ class Product {
       .then(product => {
         console.log(product);
         return product;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(result => {
+        console.log('Deleted');
       })
       .catch(err => {
         console.log(err);
