9c8abcd463033fe92f2c2b680d6984ca70b8246b
praveendan <38735067+praveendan@users.noreply.github.com>
2025-01-04 14:50:19 -0600

    section12 mongo updated teh update prod

--- section12_MongoDB/models/product.js
+++ section12_MongoDB/models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 const mongodb = require('mongodb')
 
@@ -7,7 +8,7 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = id;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
@@ -17,7 +18,7 @@ class Product {
       // Update the product
       dbOp = db
         .collection('products')
-        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+        .updateOne({ _id: this._id }, { $set: this });
     } else {
       dbOp = db.collection('products').insertOne(this);
     }
