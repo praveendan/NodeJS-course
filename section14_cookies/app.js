70b94e34cb4917ae03cfbc5e55e5b74fea066210
praveendan <38735067+praveendan@users.noreply.github.com>
2025-01-22 16:54:47 -0600

    section14 cookies added logout

--- section14_cookies/app.js
+++ section14_cookies/app.js
@@ -43,8 +43,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  // save the user in one central place and access it anywhere
-  User.findById('67873dbd94ccb92557ffd3de')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
