c9b2d3e54685ec8c1a2c5a5f70a82153dc2901e4
praveendan <38735067+praveendan@users.noreply.github.com>
2025-01-21 20:18:04 -0600

    section 14 init session management

--- section14_cookies/app.js
+++ section14_cookies/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -18,6 +19,15 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+/*
+secret: should be a long text
+resave: false session will not be saved on every response but only something changes
+saveUninitialized: no session will get saved when it does need to save
+can set max age and stuff
+*/
+app.use(
+  session({ secret: 'my secret', resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
   // save the user in one central place and access it anywhere
