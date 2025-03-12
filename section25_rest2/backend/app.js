7e55a80d36ef94b822c331801775d5b065535c5c
praveendan <38735067+praveendan@users.noreply.github.com>
2025-03-11 20:52:31 -0500

    section 25 create post

--- section25_rest2/backend/app.js
+++ section25_rest2/backend/app.js
@@ -3,18 +3,46 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
@@ -29,8 +57,10 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect('')
+  .connect('mongodb+srv://praveendan:ggRD_8huLz56bum@nodejs-course.27hsh.mongodb.net/mongoose_nodeJS_course_REST?retryWrites=true')
   .then(result => {
     app.listen(8080);
   })
   .catch(err => console.log(err));
+
+//password : ggRD_8huLz56bum
