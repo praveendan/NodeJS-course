b0ff6973c42d3dfd728ec9840cfccc795becc5bd
praveendan <38735067+praveendan@users.noreply.github.com>
2025-03-11 22:08:15 -0500

    section 25 used uuid instead of the date

--- section25_rest2/backend/app.js
+++ section25_rest2/backend/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 
 const feedRoutes = require('./routes/feed');
 
@@ -14,7 +15,7 @@ const fileStorage = multer.diskStorage({
     cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
+    cb(null, uuidv4() + '-' + file.originalname);
   }
 });
 
