48130c9599d544f2123c210d7c362f2d1d0e740a
praveendan <38735067+praveendan@users.noreply.github.com>
2025-03-23 15:01:57 -0500

    section 27 added socket IO to listen to new posts

--- section27_websocket/backend/app.js
+++ section27_websocket/backend/app.js
@@ -64,7 +64,7 @@ mongoose
   .connect('')
   .then(result => {
     const server = app.listen(8080);
-    const io = require('socket.io')(server);
+    const io = require('./socket').init(server);
     io.on('connection', socket => {
       console.log('Client connected');
     });
