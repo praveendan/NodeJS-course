cd548bcf5061aa2d191ab0304985866c946ff682
praveendan <38735067+praveendan@users.noreply.github.com>
2025-03-30 17:35:16 -0500

    section 28 graphql handled error

--- section28_graphQL/backend/app.js
+++ section28_graphQL/backend/app.js
@@ -54,7 +54,16 @@ app.use(
   graphqlHttp({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
-    graphiql: true //this will add a graphql playground at http://localhost:8080/graphql
+    graphiql: true, //this will add a graphql playground at http://localhost:8080/graphql
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || 'An error occurred.';
+      const code = err.originalError.code || 500;
+      return { message: message, status: code, data: data };
+    }
   })
 );
 
