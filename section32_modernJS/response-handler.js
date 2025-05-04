a40fd1ca7dcacbe23cd2f9775993af45281b7c32
praveendan <38735067+praveendan@users.noreply.github.com>
2025-05-03 21:44:29 -0500

    section 32 added promises support

--- section32_modernJS/response-handler.js
+++ section32_modernJS/response-handler.js
@@ -1,17 +1,20 @@
-// const fs = require('fs');
-import fs from 'fs';
+// const fs = require('fs').promises;
+import fs from 'fs/promises';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-// global variables are not available in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const resHandler = (req, res, next) => {
-  // fs.readFile('my-page.html', 'utf8', (err, data) => {
-  //   res.send(data);
-  // });
-  res.sendFile(path.join(__dirname, 'my-page.html'));
+  fs.readFile('my-page.html', 'utf8')
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // res.sendFile(path.join(__dirname, 'my-page.html'));
 };
 
 // module.exports = resHandler;
